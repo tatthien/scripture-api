9b67049ce5dd64cb277c975a8ea76838c517670c
Thien Nguyen <tatthien.contact@gmail.com>
2025-12-16 22:18:36 +0700

    feat: add logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import pino from "pino";
+import pinoHttp from "pino-http";
 import versesRouter from "./routes/verses";
 
+const logger = pino({
+  transport: process.env.NODE_ENV === 'development' ? {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss',
+      ignore: 'pid,hostname',
+    },
+  } : undefined,
+});
 const app = express();
 const port = process.env.APP_PORT || 3000
 
+app.use(pinoHttp({ logger }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,5 +30,5 @@ app.get("/health", (req, res) => {
 app.use("/verses", versesRouter);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  logger.info(`server running at http://localhost:${port}`);
 });
